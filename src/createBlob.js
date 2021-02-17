1408c66bc65d0afe31939d365408fb130270744e
Yudiel Curbelo <yudiel@mrglasswindows.com>
2021-02-16 23:12:15 -0500

    Upgrade: Gulp

--- src/createBlob.js
+++ src/createBlob.js
@@ -7,17 +7,18 @@ module.exports = function createBlob(parts, properties) {
   try {
     return new Blob(parts, properties);
   } catch (e) {
-    if (e.name !== "TypeError") {
+    if (e.name !== 'TypeError') {
       throw e;
     }
-    var BlobBuilder = window.BlobBuilder ||
-                      window.MSBlobBuilder ||
-                      window.MozBlobBuilder ||
-                      window.WebKitBlobBuilder;
+    var BlobBuilder =
+      window.BlobBuilder ||
+      window.MSBlobBuilder ||
+      window.MozBlobBuilder ||
+      window.WebKitBlobBuilder;
     var builder = new BlobBuilder();
     for (var i = 0; i < parts.length; i += 1) {
       builder.append(parts[i]);
     }
     return builder.getBlob(properties.type);
   }
-}
+};
